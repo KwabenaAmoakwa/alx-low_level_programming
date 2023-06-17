cc6ecd7253c1f799861730283bde056c6e4070c3
Kwabena Amoakwa <amoakwakwabena@gmail.com>
2023-06-17 06:15:44 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 int i, j;
@@ -8,17 +14,20 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
-
+if (i != j && i < j)
+{
 putchar('0' + i);
-putchar('0' + i);
-
+putchar('0' + j);
 if (i != 8 || j != 9)
 {
 putchar(',');
 putchar(' ');
 }
 }
+
+}
 }
 putchar('\n');
 return (0);
 }
+
