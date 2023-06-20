82215f508e4b27c670a72fa8f8a5ee0c210b6db2
Kwabena Amoakwa <amoakwakwabena@gmail.com>
2023-06-20 08:58:13 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - checks if letter is lowercase
+ * @c: the character to be checked
+ *
+ * Return: 1 is the character is lowercase. 0 otherwise
+ */
+int _islower(int c)
+{
+int i;
+i = islower(c);
+return (i);
+}
---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int r;
+
+r = _islower('H');
+_putchar(r + '0');
+_putchar('\n');
+return (0);
+}
