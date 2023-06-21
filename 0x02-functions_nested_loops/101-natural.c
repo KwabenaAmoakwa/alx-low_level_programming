7584c6681af08bedadea784151b6feb61b120946
Kwabena Amoakwa <amoakwakwabena@gmail.com>
2023-06-21 09:35:03 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int u = 0;
+int i;
+
+for (i = 0; i < 1024; i++)
+{
+if (i % 5 == 0)
+{
+u = u + i;
+}
+}
+printf("%d", u);
+}
