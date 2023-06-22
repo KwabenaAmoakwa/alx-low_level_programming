fd018bf0a9de31831e10c0675ec645b41f1e7cc1
Kwabena Amoakwa <amoakwakwabena@gmail.com>
2023-06-21 22:47:29 -0700

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,38 +1,50 @@
 #include "main.h"
-/**
- * times_table - prints multiplication table from 1 to 9
- *
- * Return: last digit of a number
- */
-void times_table(void)
-{
-int a, b, c, d, e;
 
-for (a = 0; a < 10; a++)
-{
-for (b = 0; b < 10; b++)
-{
-e = a * b;
-if (e > 9)
-{
-d = e % 10;
-c = (e - d) / 10;
-_putchar(44);
-_putchar(32);
-_putchar('0' + c);
-_putchar('0' + d);
-}
-else
-{
-if (b != 0)
-{
-_putchar(44);
-_putchar(32);
-_putchar(32);
-}
-_putchar('0' + e);
-}
-}
-_putchar('\n');
-}
-}
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
+ *
+ */
+	void print_times_table(int n)
+	{
+		int i, j, k;
+
+
+		if (n >= 0 && n <= 15)
+		{
+			for (i = 0; i <= n; i++)
+			{
+				for (j = 0; j <= n; j++)
+				{
+					k = j * i;
+					if (j == 0)
+					{
+						_putchar(k + '0');
+					} else if (k < 10 && j != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(k + '0');
+					} else if (k >= 10 && k < 100)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar((k / 10) + '0');
+						_putchar((k % 10) + '0');
+					} else if (k >= 100)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar((k / 100) + '0');
+						_putchar(((k / 10) % 10) + '0');
+						_putchar((k % 10) + '0');
+					}
+				}
+				_putchar('\n');
+			}
+		}
+	}
+
