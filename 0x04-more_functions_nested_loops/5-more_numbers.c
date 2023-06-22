05e7bd04a499a0a8937014983b7f972ed32a2e6d
Kwabena Amoakwa <amoakwakwabena@gmail.com>
2023-06-22 10:21:14 -0700

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,17 @@ void more_numbers(void)
 int i, r;
 for (i = 0; i < 10; i++)
 {
-for (r = 1; r < 15; r++)
+for (r = 0; r < 15; r++)
 {
-_putchar('0' + i);
+if (r < 10)
+{	
+_putchar('0' + r);
+}
+else if(r > 10)
+{
+_putchar('0' + (r/10));
+_putchar('0' + (r%10));
+}
 }
 _putchar('\n');
 }
