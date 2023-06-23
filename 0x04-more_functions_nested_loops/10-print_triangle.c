5a121c943372fb8acb97b5a7fd9ee56a1a58fbfa
Kwabena Amoakwa <amoakwakwabena@gmail.com>
2023-06-23 03:54:43 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle followed by a newline
+ * @size: the size of the triangle
+ *
+ */
+void print_triangle(int size)
+{
+int i, j, u;
+
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+u = size - j - 1;
+if (u > i)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+return;
+}
