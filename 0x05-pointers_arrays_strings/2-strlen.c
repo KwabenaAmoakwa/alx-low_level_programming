7010807e8bf559d2c0173baa8b1b4dfb0279c4e4
Kwabena Amoakwa <amoakwakwabena@gmail.com>
2023-06-26 07:49:00 -0700

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns lenght of a string
+ * @s: string whos length is to be returned
+ *
+ * Returns: length of string
+ *
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
