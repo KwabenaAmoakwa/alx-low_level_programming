8d91dc0ddf3c5fc9f36465bda5ec49c1ee7e057c
Kwabena Amoakwa <amoakwakwabena@gmail.com>
2023-06-26 09:10:39 -0700

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: character to be reversed
+ *
+ */
+
+void print_rev(char *s)
+{
+int len = strlen(s);
+int i;
+
+for (i = len - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
