dfe5c53240457438a983726ff5eaf3339aa6e115
Kwabena Amoakwa <amoakwakwabena@gmail.com>
2023-06-26 14:01:46 -0700

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - prints half of a string, followed by a new line.
+ * @str: string to be used
+ *
+ */
+void puts_half(char *str)
+{
+	int i;
+	int len = strlen(str);
+
+	if (len % 2 == 0)
+	{
+	for (i = (len / 2); i < len; i++)
+	{
+	_putchar(str[i]);
+	}
+	}
+	else
+	{
+		for (i = (len / 2); i < len - 1; i++)
+		{
+		_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
+
