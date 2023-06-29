f7162188e5049e0ea21c361b0b0f0113bdcf2622
Kwabena Amoakwa <amoakwakwabena@gmail.com>
2023-06-29 12:28:17 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -17,28 +17,15 @@ char *leet(char *c)
 	int len1 = strlen(low);
 	int i, k;
 
-	for (i = 0; i < len; i++)
-	{
-		if (c[i] > 96 && c[i] < 123)
-		{
-			for (k = 0; k < len1; k++)
-			{
-				if (c[i] == low[k])
-				{
-					c[i] = num[k];
-				}
-			}
-		}
-		else if (c[i] > 64 && c[i] < 91)
-		{
-			for (k = 0; k < len1; k++)
-			{
-				if (c[i] == upp[k])
-				{
-					c[i] = num[k];
-				}
-			}
-		}
-	}
-	return (c);
+for (i = 0; i < len; i++)
+{
+for (k = 0; k < len1; k++)
+{
+if ((c[i] == low[k]) || (c[i] == upp[k]))
+{
+c[i] = num[k];
+}
+}
+}
+return (c);
 }
