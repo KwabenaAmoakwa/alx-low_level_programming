32a15c7c10747581b753e0e49c05ad03e9c86501
Kwabena Amoakwa <amoakwakwabena@gmail.com>
2023-06-29 13:08:59 -0700

    6-cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @c: string
+ *
+ * Return: capitalized string
+ *
+ */
+char *cap_string(char *c)
+{
+	char seps[] = " \n,;.!?(){}\"";
+	int len = strlen(c);
+	int len1 = strlen(seps);
+	int i, b;
+
+	for (i = 0; i < len; i++)
+	{
+		if (i != len - 1)
+		{
+		for (b = 0; b < len1; b++)
+		{
+			if ((c[i] == seps[b]) && (c[i + 1] > 96 && c[i + 1] < 123))
+			{
+				c[i + 1] = c[i + 1] - 32;
+			}
+		}
+	}
+}
+return (c);
+}
