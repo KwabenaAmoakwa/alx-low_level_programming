f40157eda3747c58f8377e7c04883a0d3273b1a9
Kwabena Amoakwa <amoakwakwabena@gmail.com>
2023-06-30 09:38:10 -0700

    101-print_number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,39 +7,19 @@
  */
 void print_number(int n)
 {
-	if (n >= 1000)
+	unsigned int s1;
+
+	s1 = n;
+
+	if (n < 0)
 	{
-		_putchar('0' + n / 1000);
-		n = n % 1000;
-		_putchar('0' + n / 100);
-		n = n % 100;
-		_putchar('0' + n / 10);
-		n = n % 10;
-		_putchar('0' + n % 10);
-	}
-	else if (n >= 100 && n < 1000)
-	{
-		_putchar('0' + n / 100);
-		n = n % 100;
-		_putchar('0' + n / 10);
-		n = n % 10;
-		_putchar('0' + n % 10);
-	}
-	else if (n >= 10 && n < 100)
-	{
-		_putchar('0' + n / 10);
-		n = n % 10;
-		_putchar('0' + n % 10);
-	}
-	else if (n >= 0 && n < 10)
-	{
-		_putchar('0' + n % 10);
-	}
-	else if (n < 0)
-	{
-		n = n * -1;
 		_putchar('-');
-		_putchar('0' + n / 10);
-		_putchar('0' + n % 10);
+		s1 = -n;
 	}
+
+	if (s1 / 10 != 0)
+	{
+		print_number(s1 / 10);
+	}
+	_putchar('0' + (s1 % 10));
 }
