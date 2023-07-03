f501f02a209b2d00319f5089f7f98b849a517119
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-07-03 11:10:32 -0700

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,4 @@
-#inlcude "main.h"
+#include "main.h"
 
 /**
  * _memset - fills memory with a constant byte
@@ -10,9 +10,9 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
-	
-	for (i = 0; i < n; i ++)
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
 	{
 		*(s + i) = b;
 	}
