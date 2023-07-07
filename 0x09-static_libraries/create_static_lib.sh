df7e8681aa0a53f2433bcee929d04bbc65db61f8
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-07-07 13:54:43 -0700

    script

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-for f in *.c
-do
-
-baseName=`echo $f | cut -d "." -f 1`
-newExtension=".o"
-
-cp $f $baseName$newExtension
-
-done
+gcc -c *.c
 
 ar rcs liball.a | find . -type f -name "*.o"
