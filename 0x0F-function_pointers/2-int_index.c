962f7c2b19c301576827c4aafb8fb2dfccaf1245
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-07-20 04:15:25 -0700

    0-print

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * int_index - searches for an integer
  * @array: array
@@ -13,4 +13,8 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	array = malloc(size);
+	
+	cmp((int) *array);
+	return (array);
 }
