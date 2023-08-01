f70fa527111b6b36e86b5e3c5911871d1dc6e23e
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-08-01 10:00:55 -0700

    9-insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,6 +20,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	temp = *head;
 
+	if (idx == 0)
+	{
+		mid->next = *head;
+		*head = mid;
+		return (mid);
+	}
+
 	for (; temp != NULL; temp = temp->next)
 	{
 		if (c == idx - 1)
