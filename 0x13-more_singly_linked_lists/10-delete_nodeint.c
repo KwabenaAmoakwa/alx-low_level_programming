78a293e30689d338831bf3b9bb454fbdc6238732
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-08-01 12:37:01 -0700

    0-print

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * delete_nodeint_at_index -  deletes the node at index index of a listint_t linked list
+ * @head: list
+ * @index: index
+ *
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int c;
+
+	if (*head == NULL)
+		return (NULL);
+
+	for ()
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
  * struct listint_s - singly linked list
@@ -10,8 +11,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -24,3 +25,5 @@ int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+
+#endif
