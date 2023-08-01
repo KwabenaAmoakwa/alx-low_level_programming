87b834201ca7eae8b6cf84f32152922166a69931
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-08-01 12:13:38 -0700

    9-insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *mid, *temp;
 	unsigned int c = 0;
 
-	if (*head = NULL)
+	if (*head == NULL)
 		return (NULL);
 
 	mid = (listint_t *)malloc(sizeof(listint_t));
