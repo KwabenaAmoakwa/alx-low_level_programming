58b4992ead8e2f79ced60d39c071ed5e1be61d5d
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-12 14:37:26 -0700

    5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -14,10 +14,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	{
 		if (count == index)
 		{
-			return head;
+			return (head);
 		}
 		head = head->next;
 		count++;
 	}
-	return NULL;
+	return (NULL);
 }
