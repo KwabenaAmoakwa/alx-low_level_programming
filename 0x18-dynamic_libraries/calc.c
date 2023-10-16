9aaca10d88bc98617859323aa04dd2535ecac1aa
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-16 14:09:46 -0700

    2

---
+++ 0x18-dynamic_libraries/calc.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - returns the sum of 2 nos
+ * @a: argument
+ * @b: argument
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - returns the difference of 2 nos
+ * @a: argument
+ * @b: argument
+ *
+ * Return: difference
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - returns the product of 2 nos
+ * @a: argument
+ * @b: argument
+ *
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - returns the quotient of 2 nos
+ * @a: argument
+ * @b: argument
+ *
+ * Return: quotient
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - returns the modulo of 2 nos
+ * @a: argument
+ * @b: argument
+ *
+ * Return: quotient
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
