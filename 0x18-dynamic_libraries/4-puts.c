813f6a72571da058016977bf1f0cb692edcb7f48
KwabenaAmoakwa <amoakwakwabena@gmail.com>
2023-10-16 13:17:30 -0700

    1

--- 0x18-dynamic_libraries/4-puts.c
+++ 0x18-dynamic_libraries/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ *_puts - Entry point
+ * @s: argument
+ * 
+ * 
  *
- * Return: Always 0 (Success)
  */
 
-int main(void)
+void _puts(char *s)
 {
 puts("\"Programming is like building a multilingual puzzle");
-
-return (0);
 }
